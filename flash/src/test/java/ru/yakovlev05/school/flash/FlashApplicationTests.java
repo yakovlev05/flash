932cbd5a5c3dcfb9cdafc13b200a5f3b2bdccd8c
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-04-10 13:32:42 +0500

    Create project

---
+++ flash/src/test/java/ru/yakovlev05/school/flash/FlashApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.school.flash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
