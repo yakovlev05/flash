67a80f4d4d202c786540629eb29557de2c3a9e78
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-04-12 10:59:09 +0500

    set up swagger

---
+++ flash/src/main/java/ru/yakovlev05/school/flash/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package ru.yakovlev05.school.flash.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Flash",
+                description = "API мессенджера Flash"
+        ),
+        servers = @Server(url = "/")
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
