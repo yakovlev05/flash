e2e12d123beb53c047868f8830eefe3106d6b536
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-04-21 15:30:33 +0500

    add ws idle timeout

--- flash/src/main/java/ru/yakovlev05/school/flash/config/WebSocketConfiguration.java
+++ flash/src/main/java/ru/yakovlev05/school/flash/config/WebSocketConfiguration.java
@@ -1,10 +1,12 @@
 package ru.yakovlev05.school.flash.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 import ru.yakovlev05.school.flash.websocket.handler.ChatHandler;
 import ru.yakovlev05.school.flash.websocket.interceptor.AuthInterceptor;
@@ -26,6 +28,13 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
                 .addInterceptors(authInterceptor) // Аутентификация
                 .addInterceptors(chatInterceptor); // Извлечение нужных данных (chatId и т.п.) + проверка доступа к чату
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxSessionIdleTimeout(30 * 60 * 1000L); // Сколько сессия будет находиться в простое перед отключением
+        return container;
     }
 
 }
