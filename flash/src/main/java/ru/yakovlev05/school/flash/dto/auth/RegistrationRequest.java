0f87bdd1da5dc02778a77a6eb4beeebe13266296
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-04-24 16:56:20 +0500

    fix validation password

--- flash/src/main/java/ru/yakovlev05/school/flash/dto/auth/RegistrationRequest.java
+++ flash/src/main/java/ru/yakovlev05/school/flash/dto/auth/RegistrationRequest.java
@@ -7,7 +7,7 @@ public record RegistrationRequest(
         @Size(min = 5, message = "длина имени пользователя должна быть не менее 5")
         String username,
 
-        @Pattern(regexp = "`|~|@|\"|#|№|\\$|;|%|^|:|&|\\?|\\*|\\(|\\)|>|<|/|\\+|=|-|\\{|}|\\[|]",
+        @Pattern(regexp = ".*[~@\"#№$;%^:&?*()<>/+=-_{}\\[\\]].*",
                 message = "пароль должен содержать специальные символы")
         @Size(min = 8, message = "длина пароля должны быть не менее 8 символов")
         String password
