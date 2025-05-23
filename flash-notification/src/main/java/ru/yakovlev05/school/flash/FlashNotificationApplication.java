295a69287176b1b4260ad1c708783a4bd1d0383c
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-05-23 14:12:24 +0500

    create flash-notification project

---
+++ flash-notification/src/main/java/ru/yakovlev05/school/flash/FlashNotificationApplication.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.school.flash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashNotificationApplication.class, args);
+    }
+
+}
